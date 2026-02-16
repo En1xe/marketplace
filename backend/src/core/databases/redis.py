b1c9fbcd8f2bb87f4849738175fdc4a1a0e83a21
en1xe <en1xe924@gmail.com>
2026-02-16 19:06:55 +0300

    Ready for deploy

--- backend/src/core/databases/redis.py
+++ backend/src/core/databases/redis.py
@@ -1,12 +1,21 @@
 from functools import lru_cache
 from redis.asyncio import Redis
 
+from ..config import settings
 
 @lru_cache
 def get_rate_limiter_redis() -> Redis:
-    return Redis(host='localhost', port=6379, db=2)
+    return Redis(
+        host=settings.REDIS_HOST, 
+        port=settings.REDIS_PORT, 
+        db=settings.REDIS_RATE_LIMITER_DB
+    )
 
 
 @lru_cache
 def get_cache_redis() -> Redis:
-    return Redis(host='localhost', port=6379, db=1)
+    return Redis(
+        host=settings.REDIS_HOST, 
+        port=settings.REDIS_PORT, 
+        db=settings.REDIS_CACHE_DB
+    )
--- backend/src/main.py
+++ backend/src/main.py
@@ -9,5 +9,5 @@ app = create_application()
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True, port=8000)
+    uvicorn.run('main:app', reload=True, host='0.0.0.0', port=8000)
     
